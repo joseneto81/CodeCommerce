06f37fa02b8dec5b70ecc27dd8331da5159b64e8
Jose Neto <jose.neto@riodasostras.rj.gov.br>
2016-11-28 14:06:16 -0200

    Projeto 10 - Assets

--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -8,20 +8,14 @@
     <title>Home | E-Shop</title>
 
 <!--
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/responsive.css" rel="stylesheet">
+    <link xhref="/css/bootstrap.min.css" rel="stylesheet">
+    <link xhref="/css/font-awesome.min.css" rel="stylesheet">
+    <link xhref="/css/prettyPhoto.css" rel="stylesheet">
+    <link xhref="/css/animate.css" rel="stylesheet">
+    <link xhref="/css/main.css" rel="stylesheet">
+    <link xhref="/css/responsive.css" rel="stylesheet">
 -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/css/prettyPhoto.css" rel="stylesheet">
-    <link href="/css/animate.css" rel="stylesheet">
-    <link href="/css/main.css" rel="stylesheet">
-    <link href="/css/responsive.css" rel="stylesheet">
-
+    <link href="{{ elixir('css/all.css')}}" rel="stylesheet">
 
 </head><!--/head-->
 
@@ -154,12 +148,14 @@
 </footer><!--/Footer-->
 
 
-
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.scrollUp.min.js"></script>
-<script src="js/jquery.prettyPhoto.js"></script>
-<script src="js/main.js"></script>
+<!--
+<script ssrc="js/jquery.js"></script>
+<script ssrc="js/bootstrap.min.js"></script>
+<script ssrc="js/jquery.scrollUp.min.js"></script>
+<script ssrc="js/jquery.prettyPhoto.js"></script>
+<script ssrc="js/main.js"></script>
+-->
+    <script src="{{ elixir('js/all.js')}}"></script>
 
 </body>
 </html>
